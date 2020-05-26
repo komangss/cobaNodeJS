608015ab4efa15adaa6157aa142644762bfa7b05
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-05-26 15:37:07 +0800

    load a module

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // try to access log function in logger.js
 // remember when console log the module we have an object called exports
+
+// Load a module
+const logger = require('./logger') // if in same folder // use constant because we dont want to replace the variable
+// console.log(logger);
+// Return = 
+// { log: [Function: log] }
+
+// now we can use log function
+logger.log('Hello World')
+// Return = 
+// Hello World
