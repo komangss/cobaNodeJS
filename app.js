b0a3524ea0fd2ffa61b36b3cf2199bbc24f45d91
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-05-26 15:54:36 +0800

    working on os mofule

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-// node have few useful module build in node
-// check in documentation = https://nodejs.org/dist/latest-v12.x/docs/api/
+const os = require('os')
 
-// example fs (filesystem), http (later we learn abouth this), os (work in operating system), path, etc
-const path = require('path') // dont use ./ or ../ // node will think that was file
+let totalMemory = os.totalmem()
+let freeMemory = os.freemem()
 
-let pathObj = path.parse(__filename)
-console.log(pathObj)
-// return =
-// {
-//     root: 'C:\\',
-//     dir: 'C:\\xampp\\htdocs\\cobaNodeJS',
-//     base: 'app.js',
-//     ext: '.js',
-//     name: 'app'
-//   }
+// template String // ES6 // use bactick so dont neeed contatination use +
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
+// Return = 
+// Total Memory: 8002883584
+// Free Memory: 3531821056
+
