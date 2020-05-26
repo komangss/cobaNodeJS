f8858e969cdeaaf1fd59705a1170dc5aa64f4644
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-05-26 22:37:49 +0800

    add comment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const EventEmitter = require('events')
 
 // load the logger module and called the log function
 const Logger = require('./logger') // this is class
-const logger = new Logger()
+const logger = new Logger() // instead of using instance Event Emitter, you will use class a custom class that have defined that extend EventEmitter
 
 logger.on('messageLogged', (arg) => {
     console.log('listener called', arg)
