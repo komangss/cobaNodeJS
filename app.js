8d90803506a6fc0bb5a5b3866168ae1174a5db0d
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-05-26 15:27:28 +0800

    learn node module

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-function sayHello(name) {
-    console.log(`Hello ${name}`)
-}
+// app js is one module
+console.log(module)
+// return =
+// Module {
+//     id: '.',
+//     path: 'C:\\xampp\\htdocs\\cobaNodeJS',
+//     exports: {},
+//     parent: null,
+//     filename: 'C:\\xampp\\htdocs\\cobaNodeJS\\app.js',
+//     loaded: false,
+//     children: [],
+//     paths: [
+//       'C:\\xampp\\htdocs\\cobaNodeJS\\node_modules',
+//       'C:\\xampp\\htdocs\\node_modules',
+//       'C:\\xampp\\node_modules',
+//       'C:\\node_modules'
+//     ]
+//   }
 
-sayHello('anton')
-
-// Then Run node app.js
-// Result = 
-// Hello anton
+// module is not global object
