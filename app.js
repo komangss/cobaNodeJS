952412cbedfe2d5f2b302be5114bea3e3398c522
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-05-26 15:50:40 +0800

    Path Module

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-// var x =;
-// we have varable exports, require, module, __filename, __dirname
+// node have few useful module build in node
+// check in documentation = https://nodejs.org/dist/latest-v12.x/docs/api/
 
-console.log(exports);
-console.log(require);
-console.log(module);
-console.log(__filename);
-console.log(__dirname);
+// example fs (filesystem), http (later we learn abouth this), os (work in operating system), path, etc
+const path = require('path') // dont use ./ or ../ // node will think that was file
+
+let pathObj = path.parse(__filename)
+console.log(pathObj)
+// return =
+// {
+//     root: 'C:\\',
+//     dir: 'C:\\xampp\\htdocs\\cobaNodeJS',
+//     base: 'app.js',
+//     ext: '.js',
+//     name: 'app'
+//   }
